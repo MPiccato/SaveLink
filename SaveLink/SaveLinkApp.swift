4fa89197ff5fc114e66062166eb1d22624859847
mpiccato <mpiccato@me.com>
2022-10-22 13:17:25 -0300

    Initial Commit

---
+++ SaveLink/SaveLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinkApp.swift
+//  SaveLink
+//
+//  Created by Martin Piccato on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SaveLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
